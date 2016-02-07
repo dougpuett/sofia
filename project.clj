f7ad1868e9d647fe69579abf113d8e94abc8629f
doug <doug@pulse.me>
2016-02-07 14:00:21 -0800

    ring basics

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.3.1"]
+                 [cheshire "5.3.1"]
+                 [compojure "1.2.0"]]
+  :plugins [[lein-ring "0.8.11"]]
   :main ^:skip-aot sofia.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
